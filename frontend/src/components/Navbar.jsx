825d9c791a7226ed5f856d02987d66b5ecc957d6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-03 09:46:01 -0300

    Criando CP5

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+function Navbar() {
+  return (
+    <>
+        <header className="flex justify-between">
+            <h1>CP5</h1>
+            <ul className="flex justify-end gap-7.5">
+                <li><a href="#">Início</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
